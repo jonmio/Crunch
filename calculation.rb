f302d036f2e80c66ecfad3e3e4d68a15ee643961
Jon Mio <jon.mio@hotmail.com>
2016-07-15 13:52:46 -0400

    added comments to claculations

--- calculation.rb
+++ calculation.rb
@@ -9,10 +9,12 @@ def ci_lower_bound(positive_ratings, total_ratings, confidence)
   z = Statistics2.pnormaldist(1-(1-confidence)/2)
   phat = 1.0*positive_ratings/total_ratings
   value = (phat + z*z/(2*total_ratings) - z * Math.sqrt((phat*(1-phat)+z*z/(4*total_ratings))/total_ratings))/(1+z*z/total_ratings)
+  #make score larger
   value*10**4
 end
 
 def sum_score_method1(ratings)
+  #This calculates the score (positive reviews) of the restaurant. Excellent = 2 positive, Good = 1, any other class of reviews are not positive
   score = 0
   ratings.length.times do |index|
     case index
@@ -26,6 +28,7 @@ def sum_score_method1(ratings)
 end
 
 def sum_total_ratings(ratings)
+  #Counts total ratings, where excellent = 2, good = 1, okay =1, bad = 2, terrible = 4
   total_ratings = 0
   ratings.length.times do |index|
     case index
@@ -46,13 +49,19 @@ end
 
 def my_ranker(positive_ratings,total_ratings,standard_deviation)
   if positive_ratings > 0
+    #first term ensures that restaurant is popular enough but is asymtpotic in nature so it does not have a major impact after a certain point
+    #Second term finds ratio of positive ratings
+    #Third term subtracts the variance of the dataset. If a restaurant has very spread out reviews, this is a sign taht it is inconsistent.
+    # variance will be larger and it will score significantly lower
     return -1/(positive_ratings/60) + (positive_ratings/total_ratings)*2 - (standard_deviation)**2
   else
+    #negative infinity
     return -1.0/0.0
   end
 end
 
 def sum_score_method2(ratings)
+  #counts score of restaurant. Here, excellent = 2, good = 1, okay =0, bad = -2, terrible = -4
   score = 0
   ratings.length.times do |index|
     case index
@@ -70,6 +79,7 @@ def sum_score_method2(ratings)
 end
 
 def sd (ratings,total_ratings,positive_ratings)
+  #find standard deviation for dataset
   sq_diff = 0
   mean = positive_ratings/total_ratings
   ratings.length.times do |index|
